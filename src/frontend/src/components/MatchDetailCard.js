57fbddbc3c4d29a13c5534d425fee27d92a6cf22
Carter Walsh <Cwalsh616@gmail.com>
2021-07-16 16:32:57 -0500

    Added React Boilerplate

---
+++ src/frontend/src/components/MatchDetailCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const MatchDetailCard = () => {
+    return (
+        <div className="TeamPage">
+            <h3>Latest Matches</h3>
+            <h4>Latest Matches</h4>
+        </div>
+    )
+}
